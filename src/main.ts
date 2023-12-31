543c4014caaf16a418adc2e2e4c5e5bfbf628d75
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2024-01-01 05:16:49 +0900

    Add IPC communication to update h1 text on DOMContentLoaded event

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,8 @@ const createWindow = () => {
     },
   });
 
+  mainWindow.webContents.send('ping', 'message-from-main-process');
+
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,11 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { ipcRenderer } from "electron";
+
+ipcRenderer.on("ping", (event, message) => {
+  window.addEventListener("DOMContentLoaded", () => {
+    const h1 = document.querySelector("h1");
+    h1.innerText = message;
+  });
+});
