9dd35103fb4916865c0472375a59791cd07cf3c4
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2024-01-01 17:24:00 +0900

    contextBridgeを整えるのとコメント消す

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'path';
 
 const IS_DEV = process.env.NODE_ENV === 'development';
@@ -8,49 +8,35 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
+
+function createWindow() {
   const mainWindow = new BrowserWindow({
     webPreferences: {
-      // contextIsolation: true, // デフォルト
-      // nodeIntegration: false, // デフォルト
-      webSecurity: !IS_DEV, // 開発時のみwebSecurityを無効にする
+      webSecurity: !IS_DEV,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
+}
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools(); //必要なときだけctrl+shift+iで開けばいい
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+if (process.platform === 'darwin') {
+  app.on('window-all-closed', () => {
     app.quit();
-  }
-});
+  });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+ipcMain.handle('hello', () => {
+  console.log("hello from main");
+  return "こんにちわ from main";
+});
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,13 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from 'electron';
+
+const api = {
+  async hello() {
+    console.log("hello from preload")
+    const replyFromMain = await ipcRenderer.invoke('hello', "こんにちわ from preload");
+    console.log("reply: " + replyFromMain);
+  }
+};
+
+contextBridge.exposeInMainWorld('api', api);
+
+export type apiTypes = typeof api;
